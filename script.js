a88af1ab119b679ea7918af9900b1e3344dd3da7
AmirArsalan Zlf <arsalan.zlf@gmail.com>
2023-08-17 12:33:54 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+    const year = (new Date()).getFullYear();
+    const isAdult = people.some((person)=>{
+        return (year-person.year >=19) ? true : false;
+    })
+    console.log(isAdult);
+    
+    // Array.prototype.every() // is everyone 19 or older?
+    const everyAdult = people.every((person) =>{
+        return (year - person.year) >= 19 ? true : false
+    })
+    console.log(everyAdult);
+
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+    const searchedComment = comments.find(comment => comment.id == 823423);
+    console.log(searchedComment)
+
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+    const deletingComment = comments.findIndex((comment)=>comment.id == 823423)
+    comments.splice(deletingComment,1)
+    console.log(comments)
